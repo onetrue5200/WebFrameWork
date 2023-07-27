491d23608d0167c1392e679167efc4600665e00e
onetrue5200 <oneruexie@gmail.com>
2023-07-27 16:39:07 +0800

    encapsulate an Engine

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"wfw"
 )
 
 func main() {
-	engine := new(wfw.Engine)
-	log.Fatal(http.ListenAndServe(":9999", engine))
+	server := wfw.New()
+	server.GET("/", indexHandler)
+	server.GET("/hello", helloHandler)
+	server.Run(":9999")
+}
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
+}
+
+func helloHandler(writer http.ResponseWriter, request *http.Request) {
+	for k, v := range request.Header {
+		fmt.Fprintf(writer, "Header [%q] = %q\n", k, v)
+	}
 }
--- wfw/wfw.go
+++ wfw/wfw.go
@@ -5,25 +5,40 @@ import (
 	"net/http"
 )
 
-type Engine struct{}
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{
+		router: make(map[string]HandlerFunc),
+	}
+}
+
+func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	e.router[key] = handler
+}
+
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute(http.MethodGet, pattern, handler)
+}
+
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute(http.MethodPost, pattern, handler)
+}
 
 func (e *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	switch request.URL.Path {
-	case "/":
-		indexHandler(writer, request)
-	case "/hello":
-		helloHandler(writer, request)
-	default:
+	key := request.Method + "-" + request.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(writer, request)
+	} else {
 		fmt.Fprintf(writer, "404 NOT FOUND: %s\n", request.URL)
 	}
 }
 
-func indexHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
-}
-
-func helloHandler(writer http.ResponseWriter, request *http.Request) {
-	for k, v := range request.Header {
-		fmt.Fprintf(writer, "Header [%q] = %q\n", k, v)
-	}
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
 }
