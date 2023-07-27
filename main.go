beed7615778ac3d89b2e04923ab2ecbb4a95351d
onetrue5200 <oneruexie@gmail.com>
2023-07-27 22:09:51 +0800

    route group control

--- main.go
+++ main.go
@@ -13,17 +13,29 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
 	})
 
-	r.GET("/hello", func(c *wfw.Context) {
-		c.String(http.StatusOK, "hello %s, your're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *wfw.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello V1</h1>")
+		})
+		v1.GET("/hello", func(c *wfw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
 
-	r.GET("/hello/:name", func(c *wfw.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *wfw.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *wfw.Context) {
+			c.JSON(http.StatusOK, wfw.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/assets/*filepath", func(c *wfw.Context) {
-		c.JSON(http.StatusOK, wfw.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	log.Fatal(r.Run(":9999"))
 }
--- wfw/wfw.go
+++ wfw/wfw.go
@@ -5,17 +5,53 @@ import (
 )
 
 type Engine struct {
+	*RouteGroup
 	router *router
+	groups []*RouteGroup
+}
+
+type RouteGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouteGroup
+	engine      *Engine
 }
 
 func New() *Engine {
-	return &Engine{
+	engine := &Engine{
 		router: newRouter(),
 	}
+	engine.RouteGroup = &RouteGroup{engine: engine}
+	engine.groups = []*RouteGroup{engine.RouteGroup}
+	return engine
+}
+
+func (group *RouteGroup) Group(prefix string) *RouteGroup {
+	engine := group.engine
+	newGroup := &RouteGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouteGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	group.engine.addRoute(method, pattern, handler)
 }
 
 func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
 	e.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouteGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute(http.MethodGet, pattern, handler)
+}
+
+func (group *RouteGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute(http.MethodPost, pattern, handler)
 }
 
 func (e *Engine) GET(pattern string, handler HandlerFunc) {
