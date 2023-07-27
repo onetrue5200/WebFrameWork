216300c0d2b574108ea2efaddda4b3cfbc92f9e4
onetrue5200 <oneruexie@gmail.com>
2023-07-27 16:04:27 +0800

    standard library to launch a Web Server

--- main.go
+++ main.go
@@ -2,50 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-type Req struct {
-	A string `json:"a"`
-	B string `json:"b"`
-}
-
-func body(c *Context) {
-	data := &Req{}
-	err := c.ReadJson(data)
-	if err != nil {
-		if err = c.WriteJson(http.StatusBadRequest, nil); err != nil {
-			panic(err)
-		}
-	}
-	if err = c.WriteJson(http.StatusOK, data); err != nil {
-		panic(err)
-	}
-}
-
-func hello(c *Context) {
-	if _, err := fmt.Fprintf(c.W, "hello\n"); err != nil {
-		panic(err)
-	}
-}
-
-func headers(c *Context) {
-	for name, headers := range c.R.Header {
-		for _, h := range headers {
-			if _, err := fmt.Fprintf(c.W, "%v: %v\n", name, h); err != nil {
-				panic(err)
-			}
-		}
-	}
-}
-
 func main() {
-	server := NewHttpServer("server", MetricsFilterBuilder)
-	server.Route(http.MethodGet, "/*", hello)
-	server.Route(http.MethodGet, "/headers", headers)
-	server.Route(http.MethodPost, "/body", body)
-	err := server.Start(":8080")
-	if err != nil {
-		panic(err)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func indexHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "URL.Path = %q\n", request.URL.Path)
+}
+
+func helloHandler(writer http.ResponseWriter, request *http.Request) {
+	for k, v := range request.Header {
+		fmt.Fprintf(writer, "Header [%q] = %q\n", k, v)
 	}
 }
