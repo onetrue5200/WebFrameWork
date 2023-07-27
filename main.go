955b26474a87886de7cab7c00c18b7410b320bbc
onetrue5200 <oneruexie@gmail.com>
2023-07-28 00:08:21 +0800

    seeve static files

--- main.go
+++ main.go
@@ -2,34 +2,13 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"time"
 	"wfw"
 )
-
-func onlyForV2() wfw.HandlerFunc {
-	return func(c *wfw.Context) {
-		t := time.Now()
-		c.Fail(500, "Internal Server Error")
-		log.Printf("[%d] %s in %v for group v2", c.StatusCode, c.Request.RequestURI, time.Since(t))
-	}
-}
 
 func main() {
 	r := wfw.New()
 
-	r.Use(wfw.Logger())
-	r.GET("/", func(c *wfw.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-	})
-
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2())
-	{
-		v2.GET("/hello/:name", func(c *wfw.Context) {
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
-	}
+	r.Static("/assets", "./static")
 
 	log.Fatal(r.Run(":9999"))
 }
--- wfw/group.go
+++ wfw/group.go
@@ -1,6 +1,9 @@
 package wfw
 
-import "net/http"
+import (
+	"net/http"
+	"path"
+)
 
 type RouteGroup struct {
 	prefix      string
@@ -35,4 +38,23 @@ func (group *RouteGroup) GET(pattern string, handler HandlerFunc) {
 
 func (group *RouteGroup) POST(pattern string, handler HandlerFunc) {
 	group.addRoute(http.MethodPost, pattern, handler)
+}
+
+func (group *RouteGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+		} else {
+			fileServer.ServeHTTP(c.Writer, c.Request)
+		}
+	}
+}
+
+func (group *RouteGroup) Static(relativePath, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
 }
